d397a3de0bdc1b859da22b1e3a6233bc87cd8279
Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>
2020-08-27 14:54:28 +0200

    sample blinking code
    Signed-off-by: Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>

--- src/main.c
+++ src/main.c
@@ -1,3 +1,25 @@
+#include <avr/io.h>
+
+/* Internal RC oscillator */
+#define F_CPU 8000000UL
+
+/* External quartz oscillator */
+/* #define F_CPU 4915200UL */
+#include <util/delay.h>
+
 int main(){
+    /* Init port */
+    DDRB |= (1 << PB0);
+    PORTB |= (1 << PB0);
+
+    /* Toggle */
+    while(1){
+        PORTB &= ~(1 << PB0);
+        _delay_ms(1000);
+
+        PORTB |= (1 << PB0);
+        _delay_ms(1000);
+    }
+
     return 0;
 }
