638002d93079935dcaca990e932337113ac39e22
Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>
2020-09-21 19:53:52 +0200

    run ADC sample on timed interrupt
    Signed-off-by: Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>

--- src/adc.c
+++ src/adc.c
@@ -1,40 +1,62 @@
 #include "adc.h"
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 #define ADC_MEM ((volatile uint8_t *)0x1400)
+#define ADC_NUMBER_OF_CHANNELS 4
 
-static uint8_t m_adc_channels[4];
+static uint8_t m_adc_channels[ADC_NUMBER_OF_CHANNELS];
 
 static uint8_t adc_busy(){
     return !(PINB & (1 << PB1));
 }
 
-void adc_init(){
-    // Clear Timer/Counter prescaler
-    SFIOR |= (1 << PSR310);
-
-    // ADC frequency half of uC frequency
-    OCR0 = 0x00;
-
-    TCCR0 |= (1 << WGM01) | (1 << COM00) | (1 << CS00);
-
-    // Drive ADC clock on PB0
-    DDRB |= (1 << PB0);
-    PORTB |= (1 << PB0);
-
-    // Sample ADC busy signal on PB1
-    DDRB &= ~(1 << PB1);
-}
-
-void adc_sample(){
-    // Strobe ~WR to start conversion
+ISR(TIMER2_COMP_vect){
+    /* Strobe ~WR to start conversion */
     *ADC_MEM = 0x00;
 
     while(adc_busy());
 
-    for(uint8_t c = 0; c < 4; c++){
+    for(uint8_t c = 0; c < ADC_NUMBER_OF_CHANNELS; c++){
         m_adc_channels[c] = *ADC_MEM;
     }
+}
+
+void adc_init(){
+    /* Clear Timer/Counter1 and Timer/Counter3 prescaler */
+    SFIOR |= (1 << PSR310);
+
+    /* ADC frequency half of uC frequency */
+    OCR0 = 0x00;
+
+    /* Clear timer on compare, toggle on compare, no prescaling */
+    TCCR0 |= (1 << WGM01) | (1 << COM00) | (1 << CS00);
+
+    /* Drive ADC clock on PB0 */
+    DDRB |= (1 << PB0);
+    PORTB |= (1 << PB0);
+
+    /* Sample ADC busy signal on PB1 */
+    DDRB &= ~(1 << PB1);
+
+
+
+    /* --- Configure Timer/Counter2 for interrupt generation --- */
+
+    /* Clear Timer/Counter2 prescaler */
+    SFIOR |= (1 << PSR2);
+
+    /* Clock Timer/Counter2 from CPU clock */
+    ASSR &= ~(1 << AS2);
+
+    /* Clear timer on compare, disconnect from pins, 1024 prescaler */
+    TCCR2 = (1 << WGM21) | (1 << CS22) | (1 << CS21) | (1 << CS20);
+
+    /* 10 ms period; f_oc = f_clk / (2 * N * (1 + OCR2)) with N = 1024 */
+    OCR2 = 23;
+
+    /* Generate interrupt on timer compare match */
+    TIMSK |= (1 << OCIE2);
 }
 
 uint8_t adc_read(ADCChannel channel){
--- src/adc.h
+++ src/adc.h
@@ -9,10 +9,39 @@ typedef enum {
     ADC_CHANNEL_3
 } ADCChannel;
 
+/**
+ * @brief Initializes the ATmega to sample the external ADC
+ * with a 10 ms period.
+ *
+ * Resources used:
+ * - Timer/Counter0 is used to generate an external clock signal
+ * for the ADC module.
+ * - Timer/Counter2 is used to generate interrupts that each
+ * sample the ADC every 10 ms.
+ * - PORT B pin 0 for ADC clock.
+ * - PORT B pin 1 for ADC ~busy signal.
+ *
+ * @warning Global interrupts must be enabled outside of
+ * this function.
+ *
+ * @warning If @c adc_read is called before the first sample
+ * interrupt has been processed, the returned value will
+ * contain invalid data.
+ */
 void adc_init();
 
-void adc_sample();
-
+/**
+ * @brief Returns the last sampled value for the ADC
+ * channel determined by @p channel.
+ *
+ * @param channel The channel that a sample is requested for.
+ *
+ * @return Last sample of channel @p channel.
+ *
+ * @warning If this function is invoked before the first
+ * ADC sampling interrupt has been processed, the returned
+ * value will contain invalid data.
+ */
 uint8_t adc_read(ADCChannel channel);
 
 #endif
