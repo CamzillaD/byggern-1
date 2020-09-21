a9fcced914ffd9fbd9bb688e712e27c6f2609b47
Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>
2020-09-21 20:24:18 +0200

    simplify HID module; make ADC dependency of HID
    Signed-off-by: Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>

--- src/hid.c
+++ src/hid.c
@@ -1,108 +1,32 @@
 #include "hid.h"
-extern volatile char *ext_ADC;
-#define F_CPU 4915200UL
-#include <util/delay.h>
-#include "adc_driver.h"
+#include "adc.h"
+#include <avr/io.h>
 
+void hid_init(){
+    /* Use PD4 for left button and PD5 for right button */
+    DDRD &= ~((1 << PD4) | (1 << PD5));
 
-void hid_buttons_init(){
-
-    DDRD &= ~((1 << PD4) | ( 1 << PD5 )); //Set pins to input
-
+    /* Initialize underlying ADC resource */
+    adc_init();
 }
 
-uint8_t hid_buttons_read_left(){
-    return (PIND & (1 << PD4)) != 0;
+HidJoystick hid_joystick_read(){
+    HidJoystick v;
+    v.x = adc_read(ADC_CHANNEL_0);
+    v.y = adc_read(ADC_CHANNEL_1);
+    return v;
 }
 
-uint8_t hid_buttons_read_right(){
-    return (PIND & (1 << PD5)) != 0;
+HidSlider hid_slider_read(){
+    HidSlider v;
+    v.left = adc_read(ADC_CHANNEL_2);
+    v.right = adc_read(ADC_CHANNEL_3);
+    return v;
 }
 
-
-JoystickPosition hid_joystick_read(){
-
-    JoystickPosition pos;
-
-     // Begin conversion
-    PORTB &= ~(1 << PB2);
-    *ext_ADC = 0x01;  
-
-    PORTB |= (1 << PB2);
-    _delay_ms(1);
-
-    // Read ADC_RAM
-    PORTB &= ~(1 << PB2);
-    pos.y = *ext_ADC;
-
-
-    PORTB |= (1 << PB2);
-    _delay_ms(1);
-
-
-    // Read ADC_RAM
-    PORTB &= ~(1 << PB2);
-    pos.x = *ext_ADC;
-
-
-    PORTB |= (1 << PB2);
-    _delay_ms(1);
-
-    return pos;
-}
-
-
-SliderPosition hid_slider_read(){
-
-    SliderPosition slider;
-
-
-    /*
-   // Begin conversion
-    PORTB &= ~(1 << PB2);
-    *ext_ADC = 0x02;  
-
-    PORTB |= (1 << PB2);
-    _delay_ms(1);
-
-    // Read ADC_RAM
-    PORTB &= ~(1 << PB2);
-    slider.left = *ext_ADC;
-
-
-    PORTB |= (1 << PB2);
-    _delay_ms(1);
-
-
-    // Read ADC_RAM
-    PORTB &= ~(1 << PB2);
-    slider.right = *ext_ADC;
-
-
-    PORTB |= (1 << PB2);
-    _delay_ms(1);
-
-    */
-   
-    PORTB &= ~(1 << PB2);   // Assert ADC /CS
-    *ext_ADC = 0x82;        // Start conversion and read from CH2
-    PORTB |= (1 << PB2);    // Negate ADC /CS
-    //while(adc_busy()){};    // Wait for ADC
-    _delay_us(15);
-
-    PORTB &= ~(1 << PB2);
-    slider.left = *ext_ADC;
-    PORTB |= (1 << PB2);
-
-    PORTB &= ~(1 << PB2);
-    *ext_ADC = 0x83;
-    PORTB |= (1 << PB2);
-    //while(adc_busy()){};
-    _delay_us(15);
-    
-    PORTB &= ~(1 << PB2);
-    slider.right = *ext_ADC;
-    PORTB |= (1 << PB2);
-
-    return slider;
+HidButton hid_button_read(){
+    HidButton v;
+    v.left = ((PIND & (1 << PD4)) != 0);
+    v.right = ((PIND & (1 << PD5)) != 0);
+    return v;
 }
--- src/hid.h
+++ src/hid.h
@@ -1,36 +1,59 @@
-#ifndef HID_H 
-#define HID_H 
-#include <avr/io.h>
-
+#ifndef HID_H
+#define HID_H
+#include <stdint.h>
 
 typedef struct {
-
     uint8_t x;
     uint8_t y;
+} HidJoystick;
 
-} JoystickPosition;
-
-typedef struct{
-
+typedef struct {
     uint8_t left;
     uint8_t right;
+} HidSlider;
 
+typedef struct {
+    uint8_t left;
+    uint8_t right;
+} HidButton;
 
-} SliderPosition;
+/**
+ * @brief Initializes the Human Interface Device (HID) module,
+ * along with its underlying dependencies, including the ADC.
+ *
+ * Resources used:
+ * - PORT D pin 4 for left capacitive touch button.
+ * - PORT D pin 5 for right capacitive touch button.
+ * - All of the ADC resources.
+ *
+ * @warning Global interrupts must be enabled in order for
+ * calls to @c hid_joystick_read and @c hid_slider_read to
+ * be valid. This is because the underlying ADC relies on
+ * timed interrupts to sample these assets.
+ */
+void hid_init();
 
+/**
+ * @brief Read the joystick position.
+ *
+ * @return Position of the on-board joystick.
+ */
+HidJoystick hid_joystick_read();
 
+/**
+ * @brief Read the capacitive slider positions.
+ *
+ * @return Position of the left- and right
+ * on-board slider positions.
+ */
+HidSlider hid_slider_read();
 
-void hid_buttons_init();
+/**
+ * @brief Read the capacitive button states.
+ *
+ * @return Activation status for the left- and
+ * right on-board buttons.
+ */
+HidButton hid_button_read();
 
-uint8_t hid_buttons_read_left();
-
-uint8_t hid_buttons_read_right();
-
-JoystickPosition hid_joystick_read();
-
-SliderPosition hid_slider_read();
-
-
-
-
-#endif 
+#endif
