8c36da0dc75f557aa37d294f684e59dc6ca001a5
CamzillaD <camildy@stud.ntnu.no>
2020-11-13 04:53:24 +0100

    simplify servo module
    Signed-off-by: CamzillaD <camildy@stud.ntnu.no>

--- node2/servo.c
+++ node2/servo.c
@@ -1,7 +1,34 @@
 #include "servo.h"
+#include "sam.h"
 
+void servo_init(){
+    /* Enable peripheral clock */
+    PMC->PMC_PCER0 = PMC_PCER0_PID27;
 
-uint32_t servo_convert(double voltage){
-    //possibly assert the value
-    return (voltage - SERVO_VOLTAGE_MIN) * (1 / (SERVO_VOLTAGE_MAX - SERVO_VOLTAGE_MIN));
+    /* Hand PIO to peripheral control */
+    PIOB->PIO_PDR = PIO_PDR_P25;
+    PIOB->PIO_ABSR = PIO_ABSR_P25;
+
+    /* Enable Timer Counter clock */
+    TC0->TC_CHANNEL[0].TC_CCR = TC_CCR_CLKEN | TC_CCR_SWTRG;
+
+    /* Master clock / 128 */
+    TC0->TC_CHANNEL[0].TC_CMR = TC_CMR_TCCLKS_TIMER_CLOCK4;
+
+    /* Enable waveform generation */
+    TC0->TC_CHANNEL[0].TC_CMR = TC_CMR_WAVE
+                              | TC_CMR_WAVSEL_UP_RC
+                              | TC_CMR_ACPA_CLEAR
+                              | TC_CMR_ACPC_SET
+                              ;
+
+    /* 20 ms period */
+    TC0->TC_CHANNEL[0].TC_RC = 840000;
+
+    /* 1.5 ms duty */
+    TC0->TC_CHANNEL[0].TC_RA = 63000;
+}
+
+void servo_command_gimbal(uint8_t gimbal){
+    TC0->TC_CHANNEL[0].TC_RA = 42000 + 165 * gimbal;
 }
--- node2/servo.h
+++ node2/servo.h
@@ -2,11 +2,8 @@
 #define SERVO_H
 #include <stdint.h>
 
-#define SERVO_VOLTAGE_MIN   0.25
-#define SERVO_VOLTAGE_MAX   0.5
+void servo_init();
 
-//will use our controller output (pådrag) to calculate the correct duty cycle time
-
-uint32_t servo_convert(double voltage);
+void servo_command_gimbal(uint8_t gimbal);
 
 #endif
